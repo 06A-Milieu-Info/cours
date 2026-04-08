05b919bcd0a5d82a57e70094205ca39bc3946cae
oliviertremblay <oliviertremblay@users.noreply.github.com>
2026-04-08 14:04:31 +0000

    deploy: 73bfba0a7add556389865e60599bcfb542def52b

--- assets/travaux_tp4-ppt-ethique.md.BZEbmTg0.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as e,c as a}from"./chunks/framework.BQi7Fipq.js";const d=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"travaux/tp4-ppt-ethique.md","filePath":"travaux/tp4-ppt-ethique.md"}'),p={name:"travaux/tp4-ppt-ethique.md"};function r(o,s,c,n,i,u){return e(),a("div")}const m=t(p,[["render",r]]);export{d as __pageData,m as default};
